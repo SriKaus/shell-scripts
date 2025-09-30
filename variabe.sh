49329712a13330b0d6ee776165993d90b923da49
Kaustubh PS <kaustubh.ps.blr@gmail.com>
2025-09-30 18:43:36 +0530

    Successfully

--- variabe.sh
+++ variabe.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-dates=$(date)
-echo "The date and time of execution of command is $dates"
+dates1=$(date)
+free
+lsblk
+df -h
+uptime
+dates2=$(date)
+Totaltime=$(($dates2-$dates1))
+echo "The total time taken to execute the script is $Totaltime"
+

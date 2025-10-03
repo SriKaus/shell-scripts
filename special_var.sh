945c0df76bcc2ba6c331a8f278acc8e047fbbaaf
Kaustubh PS <kaustubh.ps.blr@gmail.com>
2025-10-03 18:18:51 +0530

    Succ

--- special_var.sh
+++ special_var.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 echo "All varaiables passed to the script : $@"
+echo "All varaiables passed to the script : $*"
+echo "Script name $0"
+echo "Current directory $PWD"
+echo "who is running this $USER"
+echo "Home directory of user: $HOME"
+echo "PID of this script $$"
+sleep 50 $
+echo "PID of the last command in the background is:$!"
+
+
+
+
+
+
+
+
+

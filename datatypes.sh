3bc4a390c98900f334822367a11456346761d66b
Kaustubh PS <kaustubh.ps.blr@gmail.com>
2025-10-03 19:34:34 +0530

    Succ

--- datatypes.sh
+++ datatypes.sh
@@ -4,3 +4,8 @@ num2=945
 Movie="Kantara"
 summ=$(($num1+$num2+$Movie))
 echo "The sum of $num1 and $num2 is $summ"
+
+Names=("Kannada","Karnataka","KGF","Kantara","Milana")
+echo "The names are : ${Names[@]}"
+echo "The first name is :${Names[1]}"
+echo "The last name :${Names[10]}"

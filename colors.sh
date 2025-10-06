10b0b09c515dd8c60dcf6dd75af299e2c4ba77c4
Kaustubh PS <kaustubh.ps.blr@gmail.com>
2025-10-06 14:28:47 +0530

    Success

--- colors.sh
+++ colors.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-Re="/e[31m"
-Gr="/e[33m"
-bl="/e[32m"
-nor="/e[0m"
+Re="\e[31m"
+Gr="\e[33m"
+bl="\e[32m"
+nor="\e[0m"
 
-echo -e "$Re This is a colured text"
+echo -e "$Re This is a colured text\n $Gr Color Color which color$nor\n Oh Wow what a surprise!"

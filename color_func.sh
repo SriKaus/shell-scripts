dd5e1d9d1e201a3d68247134a9f7d37903963a22
Kaustubh PS <kaustubh.ps.blr@gmail.com>
2025-10-06 20:49:36 +0530

    Success

---
+++ color_func.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+User=$(id -u)
+gr="\e[31m"
+re="\e[32m"
+validate(){
+    if $1 -ne 0
+    then
+    echo "Installing $2 .... $gr failed"
+    else
+    echo "Installing $2 .... $re Success"
+    fi
+}
+if [ $User -ne 0 ]
+then
+echo "$gr Please try again with root permission"
+
+else
+dnf list installed  mysql
+
+if [ $? -ne 0 ]
+then
+dnf install mysql
+validate $? "mysql"
+else
+echo "The Package mysql already exists .... Skipping Installation" 
+fi
+
+dnf list installed  mysql
+
+if [ $? -ne 0 ]
+then
+dnf install nginx
+validate $? "nginx"
+else
+echo "The Package nginx already exists .... Skipping Installation" 
+fi
+
+dnf list installed  mysql
+
+if [ $? -ne 0 ]
+then
+dnf install python3
+validate $? "python3"
+else
+echo "The Package python3 already exists .... Skipping Installation" 
+fi
+
+fi

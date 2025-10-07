2bdece6d7bb9d7a7c3c98905f9aa3572259d6a53
Kaustubh PS <kaustubh.ps.blr@gmail.com>
2025-10-07 14:15:41 +0530

    Success

---
+++ packa.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+User=$(id -u)
+gr="\e[31m"
+re="\e[32m"
+fold="/var/log/shell-scripts"
+script_name=$($0|cut -d '.' -f1)
+log_file="$fold/$script_name.logs"
+mkdir -p $fold
+
+validate(){
+    if [ $1 -ne 0 ]
+    then
+    echo -e "Installing $2 .... $gr failed"
+    else
+    echo -e "Installing $2 .... $re Success"
+    fi
+}
+if [ $User -ne 0 ]
+then
+echo -e "$gr Please try again with root permission"
+
+else
+dnf list installed  mysql>>$log_file
+
+if [ $? -ne 0 ]
+then
+dnf install mysql
+validate $? "mysql"
+else
+echo "The Package mysql already exists .... Skipping Installation" 
+fi
+
+dnf list installed  nginx>>$log_file
+
+if [ $? -ne 0 ]
+then
+dnf install nginx
+validate $? "nginx"
+else
+echo "The Package nginx already exists .... Skipping Installation" 
+fi
+
+dnf list installed  python3>>$log_file
+
+if [ $? -ne 0 ]
+then
+dnf install python3
+validate $? "python3"
+else
+echo "The Package python3 already exists .... Skipping Installation" 
+fi
+
+fi
